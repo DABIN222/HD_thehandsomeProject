157f6bbc7208e56a4330d9b09fc9555108f74e5f
parkyeomyeong <tldldh1212@naver.com>
2022-10-09 10:47:38 +0900

    페이징버튼 만들었음

--- thehandsomeProject/src/main/java/com/hdsm/domain/PageDTO.java
+++ thehandsomeProject/src/main/java/com/hdsm/domain/PageDTO.java
@@ -14,7 +14,7 @@ public class PageDTO {
 	private int total;
 	private Criteria cri;
 	
-	public void PageDTD(Criteria cri,int total) {
+	public PageDTO(Criteria cri,int total) {
 		
 		this.total = total;
 		this.cri = cri;
@@ -25,11 +25,10 @@ public class PageDTO {
 		
 		int realEnd = (int)  Math.ceil( (total * 1.0) / cri.getAmount() ) ;
 		
-		//totol을 통한 endPage 재계산
+		//total(전체 데이터수) / amount 를 한 숫자(진짜 끝 수)가 endpage보다 크면 당연히 업데이트 해줘야지
 		if ( realEnd < this.endPage) {
 			this.endPage = realEnd;
 		}//end if
-		
 		
 		//페이지 기능 활성화 여부 저장
 		this.prev = (this.startPage > 1);		
