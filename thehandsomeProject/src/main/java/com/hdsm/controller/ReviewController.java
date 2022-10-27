49e57fef229a460749e35a019077281f86aad033
nexusgh12 <j221485@gmail.com>
2022-10-27 23:51:28 +0900

    구현 커밋

--- thehandsomeProject/src/main/java/com/hdsm/controller/ReviewController.java
+++ thehandsomeProject/src/main/java/com/hdsm/controller/ReviewController.java
@@ -329,7 +329,7 @@ public class ReviewController {
 				String pid,
 				MultipartFile[] uploadFile) throws IOException {
 
-			String uploadFolder = "D:\\Backup\\Guhyeon\\Hyundai\\SecondProject\\Workspace_new\\HD_thehandsomeProject\\thehandsomeProject\\src\\main\\webapp\\resources\\review_images";			
+			String uploadFolder = "D:\\Backup\\Guhyeon\\Hyundai\\SecondProject\\secondProject_finish\\HD_thehandsomeProject\\thehandsomeProject\\src\\main\\webapp\\resources\\review_images";			
 
 			log.info(uploadFile);
 			log.info(uploadFile.length);
@@ -407,7 +407,7 @@ public class ReviewController {
 		public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
 
 			//컴퓨터마다 환경이다르므로 바꿔줘야해 !
-			String uploadFolder = "D:\\Backup\\Guhyeon\\Hyundai\\SecondProject\\Workspace_new\\HD_thehandsomeProject\\thehandsomeProject\\src\\main\\webapp\\resources\\review_images";
+			String uploadFolder = "D:\\Backup\\Guhyeon\\Hyundai\\SecondProject\\secondProject_finish\\HD_thehandsomeProject\\thehandsomeProject\\src\\main\\webapp\\resources\\review_images";
 			
 			log.info(uploadFile);
 			log.info(uploadFile.length);
@@ -436,7 +436,7 @@ public class ReviewController {
 		public ResponseEntity<Void> reviewCancle(
 				@RequestBody ReviewAttachFileDTO attachDTO){
 			
-			String deletePath = "C:/Users/kosa/Desktop/HANDSOME/HD_thehandsomeProject/thehandsomeProject/src/main/webapp";
+			String deletePath = "D:\\Backup\\Guhyeon\\Hyundai\\SecondProject\\secondProject_finish\\HD_thehandsomeProject\\thehandsomeProject\\src\\main\\webapp\\resources\\review_images";
 			
 			ReviewUtil.builder().build().deleteCancleImage(deletePath+attachDTO.getThumbPath());
 			
